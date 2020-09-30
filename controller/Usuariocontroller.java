83a6c70eabc5dfa024dbb8bcf0dc10d09bbd6017
SerenaRamos <57671444+SerenaRamos@users.noreply.github.com>
2020-09-29 22:44:27 -0300

    Inclusão de Usuários
    Controller, Repository, Model e CSS atualizados

---
+++ controller/Usuariocontroller.java
@@ -0,0 +1,81 @@
+package br.com.gerenciamentoprojetos.controller;
+//listUsuário(), addUsuário(), viewUsuario(), editUsuario(), deleteUsuario() e saveUsuario()
+
+import br.com.gerenciamentoprojetos.model.Funcionario;
+import br.com.gerenciamentoprojetos.model.Usuario;
+import br.com.gerenciamentoprojetos.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class Usuariocontroller {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @GetMapping("/usuario/list")
+    public String listUsuario(Model model) {
+
+        model.addAttribute("usuario", usuarioRepository.findAll(Sort.by("nome")));
+        return "usuario/list";
+    }
+
+    @GetMapping("/usuario/add")
+    public String addUsuario(Model model) {
+        try {
+            model.addAttribute("usuario", new Usuario());
+
+        }catch (Exception e) {
+            System.out.println("Erro:" + e.getMessage());
+        }
+        return "usuario/add";
+    }
+
+    @PostMapping("/usuario/save")
+    public String saveUsuario(Usuario usuario) {
+        try {
+            usuarioRepository.save(usuario);
+        }catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        return "redirect:/usuario/view/" + usuario.getId();
+    }
+
+    @GetMapping("/usuario/view/{id}")
+    public String viewUsuario(@PathVariable long id, Model model) {
+        model.addAttribute("usuario", usuarioRepository.findById(id));
+        return "usuario/view";
+    }
+
+    @GetMapping("/usuario/edit/{id}")
+    public String editUsuario(@PathVariable long id, Model model) {
+        model.addAttribute("usuario", usuarioRepository.findById(id));
+        return "usuario/edit";
+    }
+
+    @GetMapping("/usuario/delete/{id}")
+    public String deleteUsuario(@PathVariable long id, Model model) {
+        model.addAttribute("usuario", usuarioRepository.findById(id));
+        return "usuario/delete";
+    }
+
+    @PostMapping("/usuario/delete")
+    public String deleteUsuario(Usuario usuario) {
+        try {
+            usuarioRepository.delete(usuario);
+        }catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        return "redirect:/usuario/list";
+    }
+
+
+}
---
+++ repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamentoprojetos.repository;
+
+import br.com.gerenciamentoprojetos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+
+
+    }
+}
